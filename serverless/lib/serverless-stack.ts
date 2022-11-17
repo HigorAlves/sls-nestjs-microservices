97ad8772de5e9ef11c7103ab854f7cd6d331691e
Higor Alves <higorhaalves@gmail.com>
2022-11-17 16:25:49 -0300

    chore: nodejs function

--- serverless/lib/serverless-stack.ts
+++ serverless/lib/serverless-stack.ts
@@ -3,6 +3,7 @@ import * as path from 'path'
 import { Stack, StackProps } from 'aws-cdk-lib'
 import * as apigw from 'aws-cdk-lib/aws-apigateway'
 import * as lambda from 'aws-cdk-lib/aws-lambda'
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs'
 import { Construct } from 'constructs'
 
 export class ServerlessStack extends Stack {
@@ -10,14 +11,14 @@ export class ServerlessStack extends Stack {
 		super(scope, id, props)
 
 		const lambdaLayer = new lambda.LayerVersion(this, 'HandlerLayer', {
+			description: 'Api Handler Dependencies',
 			code: lambda.Code.fromAsset(
 				path.resolve(__dirname, '../../node_modules')
 			),
 			compatibleRuntimes: [
 				lambda.Runtime.NODEJS_14_X,
 				lambda.Runtime.NODEJS_16_X
-			],
-			description: 'Api Handler Dependencies'
+			]
 		})
 
 		const hello = new lambda.Function(this, 'HelloHandler', {
@@ -29,5 +30,33 @@ export class ServerlessStack extends Stack {
 		new apigw.LambdaRestApi(this, 'TestFunction', {
 			handler: hello
 		})
+
+		const handlerNodeJS = new NodejsFunction(this, 'Handler', {
+			runtime: lambda.Runtime.NODEJS_14_X,
+			entry: path.resolve(__dirname, '../../dist/apps/user/main.js'),
+			depsLockFilePath: '../yarn.lock',
+			handler: 'handler',
+			layers: [lambdaLayer]
+		})
+
+		const api = new apigw.RestApi(this, 'Serverless-API', {
+			restApiName: `serverless-api`,
+			description: 'api gateway',
+			deployOptions: {
+				stageName: 'default'
+			}
+		})
+		const handler = new lambda.Function(this, 'Handler', {
+			code: lambda.Code.fromAsset(
+				path.resolve(__dirname, '../../dist/apps/user')
+			),
+			handler: 'main.handler',
+			runtime: lambda.Runtime.NODEJS_16_X,
+			layers: [lambdaLayer]
+		})
+
+		api.root
+			.addResource('user')
+			.addMethod('ANY', new apigw.LambdaIntegration(handler))
 	}
 }
